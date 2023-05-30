3c7f9496b17c21be7fe2908f563528faa71a81ed
Ana <120680319+AnaSanchez95@users.noreply.github.com>
2023-05-30 13:38:35 +0200

    Change players choice

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 const sectionChoosePlayer = document.querySelector(".choose-player");
 const sectionMainContent = document.querySelector(".main-content");
 
-const divPlayer1 = document.querySelector("#player-1");
-const divPlayer2 = document.querySelector("#player-2");
+const divPlayerCross = document.querySelector("#player-1");
+const divPlayerCircle = document.querySelector("#player-2");
 
 const btnStartGame = document.querySelector("#start-game-btn");
+
 const btnPlayer1 = document.querySelector("#player-1-btn");
 const btnPlayer2 = document.querySelector("#player-2-btn");
 const btnBot1 = document.querySelector("#bot-1-btn");
 const btnBot2 = document.querySelector("#bot-2-btn");
 
+
 btnStartGame.addEventListener("click", startGame);
 
-btnPlayer1.addEventListener("click", player1Choose);
-btnPlayer2.addEventListener("click", player2Choose);
-btnBot1.addEventListener("click", chooseBot1);
-btnBot2.addEventListener("click", chooseBot2);
+btnPlayer1.addEventListener("click", () => playerCross("Player-1"));
+btnPlayer2.addEventListener("click", () => playerCircle("Player-2"));
+btnBot1.addEventListener("click", () => playerCross("Bot-1"));
+btnBot2.addEventListener("click", () => playerCircle("Bot-2"));
 
-sectionChoosePlayer.style.display = "flex";
-sectionMainContent.style.display = "none";
+
 
 const Player = (name, img) => {
     let points = 0;
@@ -26,35 +27,30 @@ const Player = (name, img) => {
     const getImg = () => img;
 }
 
+function game(){
+    sectionChoosePlayer.style.display = "flex";
+    sectionMainContent.style.display = "none";
+    btnStartGame.disabled = true;
+}
 
 function startGame(){
     sectionChoosePlayer.style.display = "none";
     sectionMainContent.style.display = "flex";
+
 }
 
-function player1Choose(){
-    divPlayer1.innerHTML = `
+function playerCross(player){
+    divPlayerCross.innerHTML = `
         <img class="cross-image" src="./assets/cruz2.png" alt=""> 
-        <input type="text" placeholder="Player 1">
+        <input type="text" placeholder="${player}" id="${player}">
         `
 }
 
-function player2Choose(){
-    divPlayer2.innerHTML = `
-        <img class="circle-image" src="./assets/Circulo.png" alt=""> 
-        <input type="text" placeholder="Player 2">
-        `
-}
-
-function chooseBot1(){
-    divPlayer1.innerHTML = `
+function playerCircle(player){
+    divPlayerCircle.innerHTML = `
         <img class="cross-image" src="./assets/cruz2.png" alt=""> 
-        <input type="text" placeholder="Bot">
+        <input type="text" placeholder="${player}" id="${player}">
         `
 }
-function chooseBot2(){
-    divPlayer2.innerHTML = `
-        <img class="circle-image" src="./assets/Circulo.png" alt=""> 
-        <input type="text" placeholder="Bot">
-        `
-}
+
+game();
